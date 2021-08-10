9957a2bb156f488b4413a433ec6efa1e28514352
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2021-08-10 08:13:42 -0500

    tables loaded

---
+++ query.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE DML;
+
+DROP TABLE IF EXISTS Employees;
+
+CREATE TABLE Employees 
+(
+Employee 	VARCHAR(10) PRIMARY KEY,
+Salary 		INT 		NOT NULL
+);
+
+INSERT INTO Employees
+(
+Employee, Salary
+)
+VALUES 	('Chris', 45000),
+		('Sally', 58000),
+		('Jack', 49000);
+
+Select	* from employees;
+
+CREATE TABLE Orders
+(
+OrderID 	INT 		PRIMARY KEY 
+						GENERATED ALWAYS AS IDENTITY,
+Employee 	CHAR(10)	REFERENCES Employees(Employee)
+						ON DELETE CASCADE
+						ON UPDATE CASCADE,
+OrderDate	DATE 		NOT NULL
+						DEFAULT NOW()
+);
+
+INSERT INTO Orders (Employee, OrderDate)
+VALUES 	('Sally', '2018-01-01'),
+		('Jack', '2018-01-01'),
+		('Sally', '2018-01-02'),
+		('Chris', '2018-01-03');
+
+select * from orders;
+
